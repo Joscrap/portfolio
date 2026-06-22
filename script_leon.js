1f650b2a4a003d70485a53033be430b39211afdf
Léon <bertrandclot@icloud.com>
2026-06-22 13:50:58 +0200

    leon modif

--- script_leon.js
+++ script_leon.js
@@ -1,11 +1,4 @@
-let nbLikes = 0;
-const boutonLike = document.getElementById("mon-bouton-like");
-if (boutonLike) {
-boutonLike.addEventListener("click", () => {
-nbLikes = nbLikes + 1;
-boutonLike.textContent = "❤ J'aime " + nbLikes;
-});
-}
+
 const btn = document.getElementById("like-btn");
 
 btn.addEventListener("click", () => {
