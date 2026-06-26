ff9a6bfbab4c49247c21a1f2bedaeb6959b0c9d6
Léon <bertrandclot@icloud.com>
2026-06-26 14:38:50 +0200

    modiflonnn

--- script_leon.js
+++ script_leon.js
@@ -4,8 +4,6 @@ const btn = document.getElementById("like-btn");
 btn.addEventListener("click", () => {
     const float = document.createElement("div");
     float.classList.add("like-float");
-
-    // Choix d’emojis façon Twitch
     const emojis = ["❤️"];
     float.textContent = emojis[Math.floor(Math.random() * emojis.length)];
 
